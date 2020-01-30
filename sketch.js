2deae290685c69b0606b30df87f33c7ffb8bdbda
Jiajia Chen <chenjengjia@gmail.com>
2020-01-30 15:03:32 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,5 @@
+let addmovie = prompt("What is your Miyazaki favorite:", "Spirited Away")
+const myArr = ['My Neighbor Totorro', 'Princess Mononoke', 'Castle in the Sky', 'Ponyo']
+console.log(myArr);
+myArr.push(addmovie);
+console.log(myArr.toString()); 
